d69f6592366bcd377865b9340fa3eb83b42c6d47
Gabriel0Ramos0 <gabrielrcorrea@outlook.com>
2025-11-03 20:28:19 -0300

    Resolvendo atividade 2

--- atividade-componentes/app/(tabs)/index.tsx
+++ atividade-componentes/app/(tabs)/index.tsx
@@ -1,18 +1,36 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function HomeScreen() {
+  const [nome, setNome] = useState('');
+
   return (
     <View style={styles.container}>
       <View style={styles.content}>
-        <Text style={styles.text}>Olá, React Native!</Text>
-        <Image
-          source={{
-            uri: 'https://cdn-icons-png.flaticon.com/512/919/919851.png',
-          }}
-          style={styles.image}
-        />
+        <View style={styles.container}>
+          <Text style={styles.text}>
+            {'Insira seu nome abaixo:'}
+          </Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Digite seu nome"
+            value={nome}
+            onChangeText={setNome}
+          />
+        </View>
+        <View style={styles.rightSide}>
+          <Text style={styles.text}>
+            {nome ? `Olá, ${nome}!` : 'Olá, visitante!'}
+          </Text>
+          <Image
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/512/919/919851.png',
+            }}
+            style={styles.image}
+          />
+        </View>
       </View>
-    </View>
+    </View >
   );
 }
 
@@ -28,9 +46,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     gap: 150,
   },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    padding: 10,
+    width: 250,
+    fontSize: 16,
+  },
+  rightSide: {
+    alignItems: 'center',
+    width: 300,
+  },
   text: {
     fontSize: 24,
     color: '#1E90FF',
+    marginBottom: 10,
   },
   image: {
     width: 200,
